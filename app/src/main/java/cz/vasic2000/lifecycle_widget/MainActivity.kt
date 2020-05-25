b89326878f2a56e95ef0c966ac069de1aec8d213
Vasic2000 <Vasic2000@yandex.ru>
2020-05-25 15:03:06 +0300

    Init

---
+++ app/src/main/java/cz/vasic2000/lifecycle_widget/MainActivity.kt
@@ -0,0 +1,12 @@
+package cz.vasic2000.lifecycle_widget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
