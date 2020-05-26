30df8fe6e420fb08bde654b29aaa3df28accc03f
Vasic2000 <Vasic2000@yandex.ru>
2020-05-26 19:10:41 +0300

    Что-то сделал, даже работает, даже с ресивером.

--- app/src/main/java/cz/vasic2000/lifecycle_widget/MyWidget.kt
+++ app/src/main/java/cz/vasic2000/lifecycle_widget/MyWidget.kt
@@ -3,52 +3,57 @@ package cz.vasic2000.lifecycle_widget
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
 
-class MyWidget : AppWidgetProvider() {
-    override fun onUpdate(context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray) {
-        //Создаем новый RemoteViews
-        val remoteViews = RemoteViews(context.getPackageName(), R.layout.my_widget)
-        //Подготавливаем Intent для Broadcast
-        val active = Intent(context, MainActivity::class.java)
-        //создаем наше событие
-        val actionPendingIntent = PendingIntent.getBroadcast(context, 0, active, 0)
-        //регистрируем наше событие
-        remoteViews.setOnClickPendingIntent(R.id.widget_button, actionPendingIntent)
 
-        //обновляем виджет
-        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews)
+class MyWidget : AppWidgetProvider() {
+    private val ACTION_SIMPLEAPPWIDGET = "ACTION_BROADCASTWIDGETSAMPLE"
+
+    fun updateAppWidget(context: Context,
+                        appWidgetManager: AppWidgetManager,
+                        appWidgetId: Int) {
+        // Construct the RemoteViews object
+        val views = RemoteViews(context.getPackageName(), R.layout.my_widget)
+        // Construct an Intent which is pointing this class.
+        val intent = Intent(context, MyWidget::class.java)
+        intent.action = ACTION_SIMPLEAPPWIDGET
+        // And this time we are sending a broadcast with getBroadcast
+        val pendingIntent = PendingIntent.getBroadcast(
+            context, 0, intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        views.setOnClickPendingIntent(R.id.widget_button, pendingIntent)
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray    ) {
+        // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
 
-    override fun onReceive(context: Context?, intent: Intent) {
-    // on receive function use this for new activity start
-        context!!.startActivity(intent)
-    }
-
-
-    companion object {
-
-        fun updateAppWidget(
-            context: Context,
-            appWidgetManager: AppWidgetManager,
-            appWidgetId: Int
-        ) {
-            val widgetText: CharSequence = context.getString(R.string.appwidget_text)
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        if (ACTION_SIMPLEAPPWIDGET == intent.action) {
+            // Construct the RemoteViews object
             val views = RemoteViews(context.getPackageName(), R.layout.my_widget)
-
-            // Здесь обновим текст, будем показывать номер виджета
-            views.setTextViewText(
-                R.id.appwidget_text,
-                String.format("%s - %d", widgetText, appWidgetId)
-            )
-            appWidgetManager.updateAppWidget(appWidgetId, views)
+            // Start new activity
+            val intt = Intent(context, MainActivity::class.java)
+            intt.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intt)
+            // This time we dont have widgetId. Reaching our widget with that way.
+            val appWidget = ComponentName(context, MyWidget::class.java)
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidget, views)
         }
     }
 }
